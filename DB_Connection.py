b4a4b82e2f102c84f2bd93775317cbfb288a4e2c
Gaio <goncalogaio12@gmail.com>
2024-05-17 18:11:02 +0100

    DB connection actualization

--- DB_Connection.py
+++ DB_Connection.py
@@ -1,16 +1,18 @@
 import psycopg2
 import maskpass
 
+password = 'password'
+
 def db_connect():
     '''
     Creates and connects to the Data Base
     '''
     decoded_pass = 0
     db = psycopg2.connect(
-        user = 'user123',
+        user = 'projectuser',
         password = int(decoded_pass),
         host = '127.0.0.1',
-        port = '1234',
-        database = 'test321'
+        port = '5432',
+        database = 'ProjectDB'
     )
     return db
