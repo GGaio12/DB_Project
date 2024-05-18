5a2d547c2707ca740f6911a7b554d55f9e7df735
Gaio <goncalogaio12@gmail.com>
2024-05-18 20:07:47 +0100

    Base postman and manager

--- Hospital_Man_Sys.py
+++ Hospital_Man_Sys.py
@@ -1,5 +1,31 @@
 from DB_Connection import db_connect
+import flask
+import logging
 
-db = db_connect()
-cursor = db.cursor()
+app = flask.Flask(__name__)
 
+StatusCodes = {
+    'success': 200,
+    'api_error': 400,
+    'internal_error': 500
+}
+
+
+
+if __name__ == '__main__':
+    # set up logging
+    logging.basicConfig(filename='log_file.log')
+    logger = logging.getLogger('logger')
+    logger.setLevel(logging.DEBUG)
+    ch = logging.StreamHandler()
+    ch.setLevel(logging.DEBUG)
+
+    # create formatter
+    formatter = logging.Formatter('%(asctime)s [%(levelname)s]:  %(message)s', '%H:%M:%S')
+    ch.setFormatter(formatter)
+    logger.addHandler(ch)
+
+    host = '127.0.0.1'
+    port = 8080
+    app.run(host=host, debug=True, threaded=True, port=port)
+    logger.info(f'API v1.0 online: http://{host}:{port}')
